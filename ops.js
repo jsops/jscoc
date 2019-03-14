57f1015e7bf18f9f8909350a543db54a059b93ec
Jade Elaine <datumdev@mail.com>
2019-03-14 19:23:28 -0400

    helloworld js

---
+++ ops.js
@@ -0,0 +1,15 @@
+// JSOPS
+
+console.log("////  JSOPS  ////")
+
+console.log("Another JS group\nthis one for *LEARNING*\n\nat SKILL::\n+ dcorp or other repurpose\n\n")
+
+
+
+console.log("\t\t***  USES  ***\n+ Projects\n+ \"show offs\"\n+ Persona \(Jade Eliane\)\n\n")
+
+
+
+
+
+//console.log("")
